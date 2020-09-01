702c5690cfedcf235be131574f6017c051651079
김정태(Jeongtae Kim)/App개발팀/11ST <origogi.kim@sk.com>
2020-09-01 16:39:14 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Flutter SNS App
+
+A new Flutter project.
+
+## Library
+
+1. AWS Cognito
+
+## Reference
+
+### Sign in / up
+
+[link](https://dribbble.com/shots/6984392-Splash-Sign-in-UX-Animation)
---
+++ lib/constant/secret.dart
@@ -0,0 +1,6 @@
+const awsUserPoolId = 'us-west-2_Y6e3yV2cB';
+const awsClientId = 'jsl65edus3a5h5ljvfe6ls4fi';
+
+const identityPoolId = 'us-west-2:e3bb5b57-026a-4066-9df1-75c21f01b377';
+const region = 'us-west-2';
+const endpoint = 'https://coazikyeong.auth.us-west-2.amazoncognito.com';
